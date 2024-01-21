7b070d0d9e7d2eec3a00b932b2e3682fa161150c
Georgij Tsábiev <gtsabiev@inbox.ru>
2024-01-21 20:08:35 +0300

    Добавляет модуль для публикации файлов из папки build в ветку gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ import svgo from 'gulp-svgmin';
 import svgstore from 'gulp-svgstore';
 import del from 'del';
 import browser from 'browser-sync';
+import ghpages from 'gh-pages';
 
 // Styles
 
@@ -165,3 +166,5 @@ gulp.series(
 server,
 watcher
 ));
+
+ghpages.publish('build', function(err) {});
